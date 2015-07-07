e3b2109fcd4197ec87b046e1517f03696f450bfd
George Borg <georgeborg00@hotmail.com>
2015-07-07 20:18:12 +0100

    updates links and DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706095551) do
+ActiveRecord::Schema.define(version: 20150707190028) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.text     "content"
+    t.string   "category"
+    t.string   "url"
+    t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
